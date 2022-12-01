ad90ffa033c8509a561490eb7c4ddbf01083457a
IshitaPathak <75848598+IshitaPathak@users.noreply.github.com>
2022-12-01 08:50:52 +0530

    Add files via upload

---
+++ JS_Tutorial/index.js
@@ -0,0 +1,54 @@
+
+//1.ways to print in java script
+// console.log("Hello World");
+// document.write("this is document write")
+
+//2.javascript console API
+// console.log("Hello World", 4 + 6, "another log");
+// console.warn("this is warning");
+// console.error("this is an error");
+// console.asset(4 == (8 - 4));
+
+//3. JavaScript Variables
+// what are variables? containers to store value
+var number1= 34;
+var number2= 31;
+console.log(number1 + number2);
+
+//4. Data type in javascript
+//numbers
+var num1= 21 ;
+var num2=34;
+
+//string
+var str1="this is a string"
+var str2="this is also a string"
+
+//object
+var marks={
+    ravi:45,
+    rohit:76,
+    rahul:56.45
+}
+
+//boolean
+var a=true;
+var b =false;
+
+// var und=undefined;
+var und;
+var v=null;
+
+console.log(a, num2);
+console.log(num1+str1);
+console.log(marks);
+console.log(und);
+
+/*At a very high level, there are two types of datatype in javascript
+1.Primitive: number1, string, undefined, null, Symbol
+2.Reference datatype or Non primitive datatype: Arrays and Objects*/
+
+var arr=[1,1,"Khushi",435,45];
+
+
+
